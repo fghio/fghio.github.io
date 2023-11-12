37972c1a7c97c52ee6ec6284b8eb6ba214552ee1
Federico Ghioldi <federico.ghioldi@polimi.it>
2023-11-12 23:08:42 +0100

    js

--- assets/js/blogCard.js
+++ assets/js/blogCard.js
@@ -7,18 +7,18 @@ $(document).ready(function () {
   let blogCards = []; // Array to store all blog cards sorted by lastUpdated
 
   function loadBlogCards() {
+    const repoOwner = 'fghio'; // replace with your GitHub username
+    const repoName = 'fghio.github.io'; // replace with your GitHub repository name
+    const path = 'posts/blog';
+
+    const apiUrl = `https://api.github.com/repos/${repoOwner}/${repoName}/contents/${path}`;
+
     $.ajax({
-      url: 'https://fghio.github.io/posts/blog/', // place where the blogCards are located
+      url: apiUrl,
       success: function (data) {
-        blogCards = [];
-        $(data)
-          .find('a')
-          .attr('href', function (i, val) {
-            console.log(val);
-            if (val.match(/\.(html)$/)) {
-              blogCards.push(val);
-            }
-          });
+        blogCards = data
+          .filter(file => file.name.endsWith('.html'))
+          .map(file => file.name);
 
         // Sort blogCards based on lastUpdated property (most recent to oldest)
         blogCards.sort(function (a, b) {
